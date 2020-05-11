5f4d28fd89319cf12ee4527f520d8e75a9efdb16
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-11 16:28:56 +0200

    Exercice 9

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+  // ajout à l'évènement click
+  $('#green').click(function(){
+    // cahngement de couleur
+    $('#text').css('color', 'green');
+  });
+});
+$(function(){
+  $('#red').click(function(){
+    $('#text').css('color', 'red');
+  });
+});
+$(function(){
+  $('#blue').click(function(){
+    $('#text').css('color', 'blue');
+  });
+});
